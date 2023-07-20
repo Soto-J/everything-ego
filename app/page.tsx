90f78d8267e40f0a011d8555001f063f92b248f1
John Soto <soto.ja415@gmail.com>
2023-07-19 21:42:32 -0400

    Adjusted background image

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,14 @@ export default function Home() {
         src="/images/feb-22-concert-4-website.jpg"
         alt="EGO"
         fill
-        style={{ objectFit: "cover",  }}
+        style={{
+          zIndex: -1,
+          objectFit: "cover",
+          filter: "brightness(0.5)",
+        }}
       />
 
-      <div className="text-neutral-300">
+      <div className=" text-neutral-300">
         <h1 className="text-3xl font-bold ">Welcome</h1>
         EVERYTHING EGO
       </div>
